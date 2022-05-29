d08fc035ce4df2a73478b4846af68cffa7cff7c0
yuichikato-oss <katochanpe.1005@softbank.ne.jp>
2022-05-29 00:10:48 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  
+
   root to: 'static_pages#top'
   get '/signup', to: 'users#new'
+  
+  # ログイン
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
 end
