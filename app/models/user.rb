5901dd523dfa62e23dc1ab5bd4a98f6f7d982c35
yuichikato-oss <katochanpe.1005@softbank.ne.jp>
2022-06-01 05:49:22 +0000

    Finish full-fledged-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name,  presence: true, length: { maximum: 50 }
@@ -10,4 +10,36 @@ class User < ApplicationRecord
                     
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  
+  def User.digest(string)
+    cost =
+    if ActiveModel::SecurePassword.min_cost
+      BCrypt::Engine::MIN_COST
+    else
+      BCrypt::Engine.cost
+    end
+    BCrypt::Password .create(string, cost: cost)
+  end
+  
+  # ランダムなトークンを返します。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続セッションのためハッシュ化したトークンをデータベースに記憶します。
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # トークンがダイジェストと一致すればtrueを返します。
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # ユーザーのログイン情報を破棄します。
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
